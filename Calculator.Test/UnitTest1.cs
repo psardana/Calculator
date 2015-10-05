9681fb98261c75a4420c780314ec082c89842c0a
psardana <piyushraw12@gmail.com>
2015-10-05 17:44:41 +0530

    changes

--- Calculator.Test/UnitTest1.cs
+++ Calculator.Test/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace Calculator.Test
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreEqual(2, 5);
+            Assert.AreEqual(5, 5);
         }
     }
 }
